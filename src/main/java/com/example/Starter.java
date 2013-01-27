0a7787a0a5e7ca4f54123cea0ce6d93768f8ad61
reta <drreta@gmail.com>
2013-01-27 17:44:27 -0500

    Embedded Tomcat 7

--- src/main/java/com/example/Starter.java
+++ src/main/java/com/example/Starter.java
@@ -1,27 +1,31 @@
 package com.example;
 
-import java.security.ProtectionDomain;
-
+import org.apache.catalina.Context;
+import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import com.example.config.AppConfig;
 
 public class Starter {
 	public static void main(final String[] args) throws Exception {
-		final ProtectionDomain domain = Starter.class.getProtectionDomain();
-
-		Tomcat tomcat = new Tomcat();
-		tomcat.setBaseDir( domain.getCodeSource().getLocation().toExternalForm() );
+		final Tomcat tomcat = new Tomcat();
 		tomcat.setPort( 8080 );
-		tomcat.addWebapp( "/", "webapp");
-		tomcat.getHost().setAppBase( domain.getCodeSource().getLocation().toExternalForm() );
-		tomcat.getHost().setAutoDeploy(true);
-		tomcat.getHost().setDeployOnStartup(true);
-		tomcat.getHost().setCreateDirs( false );
 		
-		// or we could do this for root context:
-		// tomcat.addWebapp("/", webRoot);
+		Context context = tomcat.addContext( "/", System.getProperty( "java.io.tmpdir" ) );
+		Tomcat.addServlet( context, "CXFServlet", new CXFServlet() );
+		
+		context.addServletMapping( "/rest/*", "CXFServlet" );
+		context.addApplicationListener( ContextLoaderListener.class.getName() );
+		context.setLoader( new WebappLoader( Thread.currentThread().getContextClassLoader() ) );
+		
+		context.addParameter( "contextClass", AnnotationConfigWebApplicationContext.class.getName() );
+ 		context.addParameter( "contextConfigLocation", AppConfig.class.getName() );
+ 		
 		tomcat.start();
 		tomcat.getServer().await();
-
 	}	
 }
 
